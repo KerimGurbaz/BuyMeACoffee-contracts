5db0b130c079ac656b38158b21bdf6f30c25590b
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-05-04 23:52:12 +0200

    updated

--- scripts/buy-coffee.js
+++ scripts/buy-coffee.js
@@ -12,3 +12,24 @@ async function getBalance(address){
 
   return hre.ethers.utils.formatEther(balanceBigInt);
 }
+
+// Logs the Ether balances for a list of addresses
+
+async function printBlances(addresses){
+  let idx = 0;
+  for(const address of addresses){
+    console.log(`Address ${idx} balance : `, await getBalance(address));
+    idx++
+  }
+}
+
+// Logs the memos stored on-chain from coffee purchases.
+async function printMemos(memos){
+  for(const memo of memos){
+    const timestamp = memo.timestamp;
+    const tipper = memo.name;
+    const tipperAddress = memo.from;
+    const message = memo.message;
+    console.log(`At ${timestamp}, ${tipper}, (${tipperAddress}) said: "${message}"`);
+    }
+}
