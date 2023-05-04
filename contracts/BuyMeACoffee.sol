ecdf0457ae59f5634fa285eb2e22f676494ae3eb
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-05-04 23:15:57 +0200

    withdrawTips and getMemos

--- contracts/BuyMeACoffee.sol
+++ contracts/BuyMeACoffee.sol
@@ -22,10 +22,55 @@ contract ByMeACoffee {
 
     // List of all memos received from frends.
     Memo[] memos;
-    
+
 
     // Address of contract deployer.
 
     address payable owner;
+
+    // Deploy logic.
+    constructor(){
+        owner = payable (msg.sender);
+    }
+
+/*
+    @dev buy a coffee for contract owner
+    @param _name name of the coffee buyer
+    @param _message a nice mesaage from the coffee buyer
+*/
+
+function buyCoffee(string memory _name, string memory _message) public payable{
+    require(msg.value > 0, "You can't buy coffee with 0 eth");
+// Add the memo to storage!
+    memos.push(Memo(
+        msg.sender,
+        block.timestamp,
+        _name,
+        _message
+    ));
+    // Emit log event when a new memo is creted!
+    emit NewMemo(
+        msg.sender,
+        block.timestamp,
+        _name,
+        _message
+    );
+}
+
+/*
+    @dev send the entire balance stored in this contract to the owner
+   
+*/
+function withdrawTips()public{
+    require(owner.send(address(this).balance));
+}
+
+// @dev retrieve all the memos received and stored on the blockchain
+function getMemos() public view returns(Memo[] memory){
+    return memos;
+
+}
+
+
 
 }
