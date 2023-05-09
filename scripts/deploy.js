0a675f95fbe9cf9ea2f4da77f2ea063d8b504b66
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-05-09 23:41:02 +0200

    deplo.js v-2

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,9 @@
 const hre = require("hardhat");
 async function main (){
 
+    // Get the contract to deploy
+    const BuyMeACoffee = await hre.ethers.getContractFactory("BuyMeACoffee");
+
 }
 // we recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
