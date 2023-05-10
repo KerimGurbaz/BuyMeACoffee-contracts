1975703ac16080b4ed9fb879206d9d7e0b3c65bd
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-05-10 23:47:35 +0200

    deploy.js done

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,9 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+
+const GOERLI_URL = process.env.GOERLI_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
